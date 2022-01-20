d4b91caa7e565391ea0ce61c93324012fc73270e
amandine <tao.amandine@gmail.com>
2022-01-20 17:58:55 +0100

    build and test java app with gradle

---
+++ src/test/java/com/dine/GradleTutorialTest.java
@@ -0,0 +1,11 @@
+package com.dine;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
